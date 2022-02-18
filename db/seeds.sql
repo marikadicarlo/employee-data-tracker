29e8000ca3a2fc2a5f00ab07a4c0612c7d706397
marikadicarlo <mdicarlo19@yahoo.com>
2022-02-17 22:53:11 -0500

    set up seeds.sql, dependencies, sql database

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE employee_data_trackerDB;
+
+-- Department Seeds --
+
+
+
+-- Role Seeds --
+
+
+
+-- Employee Seeds --
--- server.js
+++ server.js
@@ -3,4 +3,19 @@ const mysql = require("mysql");
 const table = require("console.table");
 const inquierer = require("inquirer");
 
-//
+// Connect to SQL database
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3006,
+    user: "root",
+    password: "",
+    database: "employee_data_trackerDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected as id ${connection.threadId}");
+    initialPrompt();
+});
+
+// Initial Prompt for User
