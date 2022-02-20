5d583d85d24e37ba89e911cf4398d5f03568355e
marikadicarlo <mdicarlo19@yahoo.com>
2022-02-20 12:31:40 -0500

    updated srver.js

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NULL,
+    salary DECIMAL(13,2) NOT NULL,
     department_id INT NOT NULL
 );
 
--- server.js
+++ server.js
@@ -5,74 +5,76 @@ const inquierer = require("inquirer");
 
 // Connect to SQL database
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "employee_data_trackerDB"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "employee_data_trackerDB",
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log(`Connected as id ${connection.threadId}`);
-    initialPrompt();
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log(`Connected as id ${connection.threadId}`);
+  initialPrompt();
 });
 
 // Initial Prompt for User
 const initialPrompt = () => {
-    inquierer.prompt([
-        {
-            type: "list",
-            name: "action",
-            message: "What would you like to do?",
-            choices: [
-                "View All Employees", 
-                "View all Roles", 
-                "View all Departments", 
-                "Add Employee", 
-                "Add Role", 
-                "Add Department", 
-                "Update Employee Role", 
-                "Exit",
-            ],
-        },
-    ]).then(answer => {
-        switch (answer.action){
-            case "View All Employees":
-            viewAllEmployees();
-            break;
+  inquierer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View all Roles",
+          "View all Departments",
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+          "Update Employee Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answer) => {
+      switch (answer.action) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
 
-            case "View All Roles":
-            viewAllRoles();
-            break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
 
-            case "View All Departments":
-            viewAllDepartments();
-            break;
+        case "View All Departments":
+          viewAllDepartments();
+          break;
 
-            case "Add Employee":
-            addEmployee();
-            break;
+        case "Add Employee":
+          addEmployee();
+          break;
 
-            case "Add Role":
-            addRole();
-            break;
+        case "Add Role":
+          addRole();
+          break;
 
-            case "Add Department":
-            addDepartment();
-            break;
+        case "Add Department":
+          addDepartment();
+          break;
 
-            case "Update Employee Role":
-            updateEmployeeRole();
-            break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
 
-            case "Exit":
-            console.log("Thanks for using my Employee Data Tracker!");
-            connection.end();
-            break;
-        }
+        case "Exit":
+          console.log("Thanks for using my Employee Data Tracker!");
+          connection.end();
+          break;
+      }
     });
-}
+};
 
 // View All Employees function
 const viewAllEmployees = (table) => {
@@ -81,7 +83,7 @@ const viewAllEmployees = (table) => {
   if (table === "department") {
     query = `SELECT * FROM department`;
   } else if (table === "ROLE") {
-    query = `SELECT R.id AS id, title, salary, D.name AS department
+    query = `SELECT R.id AS id, role.title AS title, role.salary AS salary, D.name AS department
     FROM ROLE AS R LEFT JOIN DEPARTMENT AS D
     ON R.department_id = D.id;`;
   } else {
@@ -99,38 +101,14 @@ const viewAllEmployees = (table) => {
     initialPrompt();
   });
 };
-    
+
 // View All Roles function
-const viewAllRoles = (table) => {
-    // Connect to query
-    connection.query(
-        `
-        SELECT 
-        role.id,
-        role.title AS "Title",
-        role.salary AS "Salary"
-        department.name AS "Department"
-        FROM role
-        LEFT JOIN department ON role.department_id = department.id;
-        `,
-        (err, res) => {
-            if (err) throw err;
-            // Display query results using console.table
-            console.table(res);
-            initialPrompt();
-        }
-    );
-};
+// Enter code here
 
 
 // View All Departments
-function viewAllDepartments() {
-  var query = "SELECT * FROM department";
-  connection.query(query, function (err, res) {
-    console.table(res);
-    initialPrompt();
-  });
-}
+// Enter code here
+
 
 
 // Add Employee function
@@ -222,7 +200,7 @@ const addEmployee = () => {
 // Add Role
 const addRole = () => {
   //get the list of all department with department_id to make the choices object list for prompt question
-  const departments = [];
+  const department = [];
   connection.query("SELECT * FROM department", (err, res) => {
     if (err) throw err;
 
@@ -231,7 +209,7 @@ const addRole = () => {
         name: dep.name,
         value: dep.id,
       };
-      departments.push(qObj);
+      department.push(qObj);
     });
 
     //question list to get arguments for making new roles
@@ -249,7 +227,7 @@ const addRole = () => {
       {
         type: "list",
         name: "department",
-        choices: departments,
+        choices: department,
         message: "Which department is this role in?",
       },
     ];
@@ -302,7 +280,6 @@ const addDepartment = () => {
       console.error(err);
     });
 };
-
 
 // Update Employee Role function
 const updateEmployeeRole = () => {
